45cac84ae9066e8e175deb8a8bb23d7fd7d8f6bb
Jananisri14 <jananisathishkumar380@gmail.com>
2026-04-01 12:22:29 +0530

    Selenium Practice

--- selenium_java_new/src/test/java/selenium_java/selenium_java_new/Demo2.java
+++ selenium_java_new/src/test/java/selenium_java/selenium_java_new/Demo2.java
@@ -1,11 +1,9 @@
 package selenium_java.selenium_java_new;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
 public class Demo2 {
 	public static void main(String[] args) {
 		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("https://www.flipkart.com");
 		String title=driver.getTitle();
 		int len=title.length();
