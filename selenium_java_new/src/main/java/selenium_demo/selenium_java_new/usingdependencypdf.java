136be1646f0e998f03b8613e12f98e48a3408ab6
Jananisri14 <jananisathishkumar380@gmail.com>
2026-05-11 17:09:31 +0530

    11-05-2026

--- selenium_java_new/src/main/java/selenium_demo/selenium_java_new/usingdependencypdf.java
+++ selenium_java_new/src/main/java/selenium_demo/selenium_java_new/usingdependencypdf.java
@@ -1,10 +1,8 @@
 package selenium_demo.selenium_java_new;
 import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
@@ -13,50 +11,76 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class usingdependencypdf {
 
-	public static void main(String[] args) throws InterruptedException {
-		String downloadFilepath = System.getProperty("user.dir")+ File.separator + "downloads";
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-notifications");
-		options.addArguments("--disable-popup-blocking");
-		options.addArguments("--disable-extensions");
-		Map<String, Object> prefs = new HashMap<>();
-		prefs.put("plugins.always_open_pdf_externally", true);
-		prefs.put("download.default_directory", downloadFilepath);
-		prefs.put("download.prompt_for_download", false);
-		options.setExperimentalOption("prefs", prefs);
-		WebDriver driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf");
-		Thread.sleep(5000);
-		File downloadedFile = new File(downloadFilepath + File.separator + "dummy.pdf");
-		if (downloadedFile.exists()) {
-			System.out.println("File downloaded successfully");
-			} 
-		else {
+public static void main(String[] arg)
+	
+	{
+		String down_path = System.getProperty("user.dir") + File.separator + "downloads";
 
-			System.out.println("File not downloaded");
-			driver.quit();
-			return;
+		ChromeOptions options = new ChromeOptions();
+
+		Map<String, Object> prefs = new HashMap<>();
+
+		prefs.put("plugins.always_open_directory", true);
+
+		prefs.put("download.default_directory", down_path);
+
+		options.setExperimentalOption("prefs", prefs);
+
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.manage().window().maximize();
+
+		driver.get("https://demo.automationtesting.in/FileDownload.html?utm_source=chatgpt.com");
+
+		WebElement downLink = driver
+				.findElement(By.xpath("//div[@style=\"text-align: center;\"]/a[text()=\"Download\"]"));
+
+		downLink.click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		
+		
+
+		File downFile = new File(down_path + "/samplefile.pdf");
+		
+		wait.until((ExpectedCondition<Boolean>) wd -> downFile.exists());
+		
+		if (downFile.exists()) 
+		
+		{
+			System.out.println("File downloaded successfully: " + downFile.getAbsolutePath());
+		} else 
+		
+		{
+			System.out.println("File download failed.");
+		}
+		
+		driver.quit();
+		
+		
+		PDDocument document = null;
+		
+		try
+		{
+			document = Loader.loadPDF(downFile);
+			PDFTextStripper stripper = new PDFTextStripper();
+			String pdfText = stripper.getText(document);
+			document.close();
+			
+			System.out.println("Extracted text from PDF:");
+			System.out.println(pdfText);
+			
+		}
+		catch (Exception e) 
+		{
+			System.out.println("An error occurred while reading the PDF file."+ e.getMessage());
+			e.printStackTrace();
 		}
 
-		PDDocument document = null;
-
-		try {
-			document = Loader.loadPDF(downloadedFile);
-			PDFTextStripper pdfStripper = new PDFTextStripper();
-			String text = pdfStripper.getText(document);
-			System.out.println("Text in PDF:");
-			System.out.println(text);
-			document.close();
-			} catch (IOException e) {
-				System.out.println("An error occurred while reading the PDF file");
-				e.printStackTrace();
-				}
-
-		driver.quit();
 	}
 }
